a80320e20df22afeec53fcd858efc322ab0a2f49
Gussephe_Benjamin <107523472+gussephe-benjamin@users.noreply.github.com>
2025-06-09 17:24:56 -0500

    Add files via upload

---
+++ operadores en python/modulo_analisis.py
@@ -0,0 +1,56 @@
+x = 7 
+
+y = 2
+
+print(x/y)
+
+print(y/y)
+
+# varía el residuo porque 
+ 
+print(x%y)
+
+print(y%x)
+
+a = 5
+
+
+b = 3
+
+# 5 / 3 = 1,666
+
+print(a/b)
+
+# 5 - 3 = 2
+
+print(a%b)
+
+# 3 / 5 = 0,6
+
+print(b/a)
+
+# residuo
+
+# 3 - 0
+
+print(b%a)
+
+# Que es el residuo?? basicamente para un numero x , y
+# residuo = x - y * (cantidad multiplicada por y de manera que sea mayor o igual que x)
+
+print(143%5)
+
+# print(5*28) = 140
+
+# porque no  29?, 29 sería 145 y esto se sale de la condición inicial. 
+
+# residuo = 143 - (5 x 28) = 143 - 140 = 3
+
+
+
+
+
+
+
+
+
